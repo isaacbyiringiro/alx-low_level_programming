b68c821e1b36c8aa590d0ca8135a94ea3ac93805
isaacbyiringiro <atbyiringiro999@gmail.com>
2023-08-11 08:44:46 -0700

    my first work of checking whether number is o + or -

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,22 +5,22 @@
 /* betty style doc for function main goes there */
 int main(void)
 {
-	int n;
+        int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
+        srand(time(0));
+        n = rand() - RAND_MAX / 2;
+        /* your code goes there */
 
-	/* Check if number is zero, positive or negative */
-	if (n > 0) {
-		printf("The number %d is positive.\n, n");
-	}
-	else if (n < 0) {
-		printf("The number %d is negative.\n, n");
-	}
-	else (n = 0) {
-		printf("The numbe %d is zero.\n, n");
-	}
-	return (0);
+        /* Check if number is zero, positive or negative */
+        if (n > 0) {
+                printf("The number %d is positive.\n, n");
+        }
+        else if (n < 0) {
+                printf("The number %d is negative.\n, n");
+        }
+        else {
+                printf("The numbe %d is zero.\n, n");
+        }
+        return (0);
 
-}
+}}
